cfd64c426ededaaf51fe6fa6ed43823dfedfa8f6
Spencer <spencerchangtw@gmail.com>
2025-06-05 08:41:16 +0800

    Add SAMD21 Tetris skeleton

---
+++ src/button.c
@@ -0,0 +1,11 @@
+#include "button.h"
+#include <samd21.h>
+
+void button_init(void) {
+    /* TODO: configure GPIO pins for buttons */
+}
+
+uint8_t button_get_state(void) {
+    /* TODO: read GPIO pin states and return bitmask */
+    return 0;
+}
---
+++ src/button.h
@@ -0,0 +1,9 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <stdint.h>
+
+void button_init(void);
+uint8_t button_get_state(void);
+
+#endif /* BUTTON_H */
